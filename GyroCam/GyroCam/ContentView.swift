b1c53204de79540d9862aae2c571365862f35515
joycelim98 <55239913+joycelim98@users.noreply.github.com>
2023-01-09 10:41:25 +0800

    Add files via upload
    Init files for gyrocam

---
+++ GyroCam/GyroCam/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GyroCam
+//
+//  Created by Joyce Lim Li Jie on 9/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GyroCam/GyroCam/GyroCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyroCamApp.swift
+//  GyroCam
+//
+//  Created by Joyce Lim Li Jie on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GyroCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
